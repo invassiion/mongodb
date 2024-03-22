3133a8e02ad6c494a5d308d80b7ccc11d0dcb883
Invasiion <invassiion@yandex.ru>
2024-03-22 16:21:04 +0700

    init commit

---
+++ src/main/java/com/example/mongo/user/entity/UserDoc.java
@@ -0,0 +1,18 @@
+package com.example.mongo.user.entity;
+
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDoc {
+    private ObjectId id;
+    private String firstName;
+    private String lastName;
+
+}
